39855f8ea599a94c0a187b506dbd1a141d5007b2
shindong96 <2006peter@naver.com>
2024-06-10 02:43:02 +0900

    refactor: Nutrition 생성자 가독성을 위해 빌더 패턴 적용

--- src/main/java/com/sakak/precourse/domain/Nutrition.java
+++ src/main/java/com/sakak/precourse/domain/Nutrition.java
@@ -6,15 +6,17 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Entity
 @Table(name = "nutrition")
 @NoArgsConstructor
+@AllArgsConstructor
 @Getter
-@Setter
+@Builder
 public class Nutrition {
 
     @Id
