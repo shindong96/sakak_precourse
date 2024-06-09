1269a283155b6524bb57267927e1568242c45fb5
shindong96 <2006peter@naver.com>
2024-06-09 17:25:12 +0900

    spring init

---
+++ src/main/java/com/sakak/precourse/PrecourseApplication.java
@@ -0,0 +1,13 @@
+package com.sakak.precourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrecourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrecourseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sakak/precourse/PrecourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sakak.precourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrecourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
