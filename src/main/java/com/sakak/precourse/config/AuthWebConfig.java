428d6088b79b1be856caaed599efaba31d1ad4dc
shindong96 <2006peter@naver.com>
2024-06-10 12:37:27 +0900

    feat: cors 설정

---
+++ src/main/java/com/sakak/precourse/config/AuthWebConfig.java
@@ -0,0 +1,18 @@
+package com.sakak.precourse.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class AuthWebConfig implements WebMvcConfigurer {
+    private static final String ALLOWED_METHOD_NAMES = "GET,HEAD,POST,PUT,DELETE,TRACE,OPTIONS,PATCH";
+
+    @Override
+    public void addCorsMappings(final CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedMethods(ALLOWED_METHOD_NAMES.split(","))
+                .exposedHeaders("*");
+    }
+}
