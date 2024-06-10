78956111c140e8d7f679f3d8abe7f80d95a4d6b1
shindong96 <2006peter@naver.com>
2024-06-10 15:18:59 +0900

    refactor: 필요없는 애노테이션 제거

--- src/main/java/com/sakak/precourse/domain/Nutrition.java
+++ src/main/java/com/sakak/precourse/domain/Nutrition.java
@@ -1,6 +1,5 @@
 package com.sakak.precourse.domain;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -28,10 +27,7 @@ public class Nutrition {
     private String dbGroup;
     private String commercialProduct;
     private String foodName;
-
-    @Column(name = "research_year")
     private String researchYear;
-
     private String makerName;
     private String collectionTime;
     private String foodCategory;
